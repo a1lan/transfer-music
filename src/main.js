63eb1391b0d7dbb187284772aa5ed6664b8541a0
a1lan <allanmathew1@gmail.com>
2024-06-20 20:38:16 +0100

    Now shows tracks in a playlist

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
-import fs from 'fs'
+import { fail } from "assert";
+import fs from "fs";
 
 const ACCESS_TOKEN = JSON.parse(fs.readFileSync("config.json")).accessToken;
 
 let playlists = await getPlaylists();
-console.log(playlists);
+
+console.log("My Playlists:");
+for (let i = 0; i < playlists.length; i++) {
+  console.log(`  ${i}: ${playlists[i].name}`);
+}
+
+let tracks = await getTracks(playlists, 8);
+console.log("\nTracks:");
+for (let i = 0; i < tracks.length; i++) {
+  console.log(`  ${i}: ${tracks[i].track.name}`);
+}
 
 // ------------ FUNCTIONS ------------
 
@@ -17,7 +28,19 @@ async function getPlaylists() {
   let playlists = await fetch(
     "https://api.spotify.com/v1/me/playlists",
     requestParameters
-  )
-    .then((res) => res.json());
-  return playlists;
+  ).then((res) => res.json());
+  return playlists.items;
+}
+
+async function getTracks(playlists, index) {
+  let uri = playlists[index].tracks.href;
+  uri += "?limit=100";
+  let requestParameters = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${ACCESS_TOKEN}`,
+    },
+  };
+  let tracks = await fetch(uri, requestParameters).then((res) => res.json());
+  return tracks.items;
 }
